1704f5400e0da5d9720d425bd314e60dd012ecae
Sayne Rojas <saynegrojas@yahoo.com>
2019-10-30 06:39:37 -0700

    changes in email.js

--- email.js
+++ email.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 // config for mailgun
 const auth = {
     auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.DOMAIN_KEY
+        api_key: '0dff9f14416077f3ae74e7ce498e78a1-816b23ef-c4401e4b',
+        domain: 'sandbox9f71f28f06d947c2b9e87f0f1f0c3f0f.mailgun.org'
     }
 }
 const transporter = nodemailer.createTransport(mailGun(auth));
