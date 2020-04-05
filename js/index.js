a6906423f3b1d860c47e8ce2d344864b8d1bc354
Sayne Rojas <saynegrojas@yahoo.com>
2020-04-04 22:46:11 -0700

    cleaned js commenting

--- js/index.js
+++ js/index.js
@@ -47,7 +47,6 @@ $(function () {
         form[0].reset();
     })
 
-
     // PROJECT
     const tl = new TimelineMax();
 
@@ -69,17 +68,17 @@ $(function () {
     //new wow animation
     var wow = new WOW(
         {
-            boxClass: 'wow',      // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset: 0,          // distance to the element when triggering the animation (default is 0)
-            mobile: true,       // trigger animations on mobile devices (default is true)
-            live: true,       // act on asynchronously loaded content (default is true)
+            boxClass: 'wow',            // animated element css class (default is wow)
+            animateClass: 'animated',   // animation css class (default is animated)
+            offset: 0,                  // distance to the element when triggering the animation (default is 0)
+            mobile: true,               // trigger animations on mobile devices (default is true)
+            live: true,                 // act on asynchronously loaded content (default is true)
             callback: function (box) {
                 // the callback is fired every time an animation is started
                 // the argument that is passed in is the DOM node being animated
             },
-            scrollContainer: null,    // optional scroll container selector, otherwise use window,
-            resetAnimation: true,     // reset animation on end (default is true)
+            scrollContainer: null,      // optional scroll container selector, otherwise use window,
+            resetAnimation: true,       // reset animation on end (default is true)
         }
     );
     //initiate wow animation
